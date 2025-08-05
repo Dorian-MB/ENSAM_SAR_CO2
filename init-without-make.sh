18da3e49c17b32006609835043e6e571fa588100
Dorian-MB <dorian.mariani@gmail.com>
2025-08-05 18:19:52 +0200

    del some file

--- init-without-make.sh
+++
@@ -1,8 +0,0 @@
-
-# Build executable without make
-
-poetry run python -m PyInstaller --onefile --add-data "KPIS\template.html;KPIS\\" configurable_main.py
-
-mkdir -p dist/
-cp config.yaml dist/config.yaml
-
